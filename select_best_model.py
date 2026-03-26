670c51263c9df8ab7052486c8b413eeb671341da
adithi2000 <adithikarthi29@gmail.com>
2026-03-26 19:48:06 +0530

    changes in fast api processing

--- select_best_model.py
+++ select_best_model.py
@@ -2,14 +2,19 @@ from dotenv import load_dotenv
 import mlflow
 import os
 
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+
 # mlflow.set_experiment(os.getenv("EXPERIMENT_NAME"))
 
 def select_best_model():
     # Fetch all runs for the experiment
+    load_dotenv()
+    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+    exp=mlflow.search_experiments()
+    for e in exp:
+        print(e.name)
     experiment = mlflow.get_experiment_by_name(os.getenv("EXPERIMENT_NAME"))
     runs = mlflow.search_runs(experiment_ids=[experiment.experiment_id])
-    runs=runs.dropna(subset=['metrics.best_score'])
+    runs=runs.dropna(subset=['metrics.best_val_score'])
 
     # Find the run with the best validation accuracy
     best_run = runs.loc[runs['metrics.best_val_score'].idxmax()]
@@ -18,7 +23,7 @@ def select_best_model():
     print(f"Best Validation Score: {best_run['metrics.best_val_score']}")
 
     # Load the best model artifact
-    best_model_uri = f"runs:/{best_run['run_id']}/model"
+    best_model_uri = f"runs:/{best_run['run_id']}/best_model"
     best_model = mlflow.pytorch.load_model(best_model_uri)
 
     return best_model
