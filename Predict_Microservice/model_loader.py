f95b0575494bb1c53044e93f32cf3e42f3dd30c2
adithi2000 <adithikarthi29@gmail.com>
2026-05-29 21:20:35 +0530

    changes to model loader location

--- Predict_Microservice/model_loader.py
+++ Predict_Microservice/model_loader.py
@@ -10,12 +10,13 @@ import torch
 def load_model():
     best_model_uri = select_best_model()
     print(f"Loading model from URI: {best_model_uri}")
+    device=None
     if torch.cuda.is_available():
         device = torch.device("cuda")
         print("Using GPU for model loading.")
     else:
         device = torch.device("cpu")
         print("Using CPU for model loading.")
-    model = mlflow.pytorch.load_model(best_model_uri)
-    model.to(device)
+    model = mlflow.pytorch.load_model(best_model_uri, map_location=device)
+    # model.to(device)
     return model
